cfdda4efddae83c671225f19c95f50cac00fef87
Mohammad Tauqir <tauqir295@gmail.com>
2019-10-05 18:34:24 +0530

    Update IPosition.java

--- src/com/abyat/mvp/interfaces/IPosition.java
+++ src/com/abyat/mvp/interfaces/IPosition.java
@@ -1,19 +1,10 @@
 package com.abyat.mvp.interfaces;
 
 /**
- * Player position, to be implemented for the concrete game.
+ * Player position, to be implemented in the game.
  *
  * @author Mohammad Tauqir
  */
 public interface IPosition<A extends IAction> {
-
-	/**
-	 * Calculates player rating based on player position, action and action times.
-	 *
-	 * @param action Player action
-	 * @param times Times repeated
-	 *
-	 * @return rating
-	 */
-	public int getPlayerRating(A action, int times);
+    int getPlayerRating(A action, int times);
 }
